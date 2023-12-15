ecda1a174a2eab01fcca25ffffb8732beae02c79
tracysgit <tracy@wildiris.net>
2023-12-15 14:00:55 -0600

    Add recent updates.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    format: 'file',
+    inlineStylesheets: 'never',
+  },
+  compressHTML: false,
+  output: 'static',
+  trailingSlash: 'never',
+
+});
